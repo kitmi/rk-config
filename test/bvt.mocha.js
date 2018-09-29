8b435e0664de42d5779c2465e230633e8aa95e04
Rockie Guo <rockie@kitmi.com.au>
2018-09-29 12:26:56 +1000

    Add jsv test case.

--- test/bvt.mocha.js
+++ test/bvt.mocha.js
@@ -65,7 +65,8 @@ describe('bvt', function () {
                 cfg['key2']['object'].should.have.keys('non', 'itpl', 'swig');
                 cfg['key2']['object']['non'].should.equal('nothing');
                 cfg['key2']['object']['swig'].should.equal('10');
-                cfg['key2']['js'].should.be.exactly(200);
+                cfg['key2']['jsv1'].should.be.exactly(200);
+                cfg['key2']['jsv2'].should.equal('Bob Sydney');
 
                 done();
 
